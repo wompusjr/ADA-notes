831ea4d8042cd33d9f5421fad6e790815040d3aa
wompusjr <june.burke@utexas.edu>
2025-01-30 17:04:23 -0600

    local commit 17:04-1:30

--- 1:30.R
+++ 1:30.R
@@ -26,3 +26,36 @@ d2 <- d2 %>%
 d3 <- read_csv("/Users/juneburke/Downloads/KamilarAndCooperData.csv", col_names = TRUE)
 d3
 names(d3)
+detach(d3)
+mean(d3$Brain_Size_Species_Mean, na.rm = TRUE)
+attach(d3) #allows you to not have to specify which df you're talking about
+mean(Brain_Size_Female_Mean)
+detach(d3)
+with(d3,
+     mean(Body_mass_female_mean, na.rm = TRUE),
+     ) #with is a function that allows you to not specify which df but w/out attached
+summary (d3)
+install.packages("skimr")
+library(skimr)
+skim(d3)
+library(ggplot2)
+boxplot(log(d3$Body_mass_female_mean))
+stripchart(log(d3$Body_mass_female_mean),
+           method = "jitter",
+           col = "blue",
+           vertical = TRUE,
+           add = TRUE)
+boxplot(log(d3$Body_mass_female_mean) ~ d3$Family)
+stripchart(log(d3$Body_mass_female_mean) ~ d3$Family,
+           method = "jitter",
+           col = "blue",
+           vertical = TRUE,
+           add = TRUE)
+p <- ggplot(data = d3,
+       aes(x = d3$Family, y = log(d3$Body_mass_female_mean))) + 
+  geom_boxplot(na.rm = TRUE) + 
+  geom_jitter( color = "blue", width = 0.1)
+p
+
+
+       
